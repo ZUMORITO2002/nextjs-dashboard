4378d75d9469e84617f1dc90043364ca370c0b12
ZUMORITO2002 <gomezandrin@gmail.com>
2024-02-12 16:38:55 +0530

    Update page.tsx

--- app/dashboard/customers/create/page.tsx
+++ app/dashboard/customers/create/page.tsx
@@ -1,23 +1,27 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import
+// import Form from '@/app/ui/invoices/create-form';
+// import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+
  
-export default async function Page() {
-  const customers = await fetchCustomers();
+// export default async function Page() {
+//   const customers = await fetchCustomers();
  
-  return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Customers', href: '/dashboard/customers' },
-          {
-            label: 'Add Customer',
-            href: '/dashboard/customers/create',
-            active: true,
-          },
-        ]}
-      />
-      <Form customers={customers} />
-    </main>
-  );
+//   return (
+//     <main>
+//       <Breadcrumbs
+//         breadcrumbs={[
+//           { label: 'Customers', href: '/dashboard/customers' },
+//           {
+//             label: 'Add Customer',
+//             href: '/dashboard/customers/create',
+//             active: true,
+//           },
+//         ]}
+//       />
+//       <Form customers={customers} />
+//     </main>
+//   );
+// }
+export default function Page()
+{
+  return "Hello India"
 }

17f8519b4e7f9f157591ac5ea04dbb12e27c5773
ZUMORITO2002 <gomezandrin@gmail.com>
2024-03-04 18:03:41 +0530

    fileupload

--- app/dashboard/quotation/create/page.tsx
+++ app/dashboard/quotation/create/page.tsx
@@ -1,7 +1,16 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchQuotation } from '@/app/lib/data';
- 
+import ImageUpload from "@/app/ui/quotation/image-upload";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 export default async function Page() {
   const customers = await fetchQuotation();
  
@@ -18,6 +27,31 @@ export default async function Page() {
         ]}
       />
       <Form customers={customers} />
+      <h1 className="text-5xl font-semibold justify-center al">OR</h1>
+      <p className="text-muted-foreground">File upload component for React</p>
+      <div className="flex gap-4">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="rounded-full shadow" variant="outline">
+              File upload
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="text-center">
+                Upload your files
+              </DialogTitle>
+              <DialogDescription className="text-center">
+                The only file upload you will ever need
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <ImageUpload />
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
     </main>
   );
 }
+

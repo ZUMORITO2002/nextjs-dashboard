2ae1cbe0d07eb34a421f60e114506bdb8ce1676f
Onil17 <94481475+Onil17@users.noreply.github.com>
2024-03-13 21:32:35 +0530

    Update create-form.tsx

--- app/ui/suppliers/create-form.tsx
+++ app/ui/suppliers/create-form.tsx
@@ -1,12 +1,12 @@
-"use client"
+'use client';
 
-import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useFieldArray, useForm } from "react-hook-form"
-import { z } from "zod"
+import Link from 'next/link';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
@@ -15,74 +15,74 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { Textarea } from "@/components/ui/textarea"
-import { toast } from "@/components/ui/use-toast"
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { toast } from '@/components/ui/use-toast';
 
 const profileFormSchema = z.object({
   username: z
     .string()
     .min(2, {
-      message: "Username must be at least 2 characters.",
+      message: 'Username must be at least 2 characters.',
     })
     .max(30, {
-      message: "Username must not be longer than 30 characters.",
+      message: 'Username must not be longer than 30 characters.',
     }),
   email: z
     .string({
-      required_error: "Please select an email to display.",
+      required_error: 'Please select an email to display.',
     })
     .email(),
   bio: z.string().max(160).min(4),
   urls: z
     .array(
       z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
+        value: z.string().url({ message: 'Please enter a valid URL.' }),
+      }),
     )
     .optional(),
-})
+});
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
+  bio: 'I own a computer.',
   urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
+    { value: 'https://shadcn.com' },
+    { value: 'http://twitter.com/shadcn' },
   ],
-}
+};
 
 export function SuppliersProfileForm() {
   const form = useForm<ProfileFormValues>({
     resolver: zodResolver(profileFormSchema),
     defaultValues,
-    mode: "onChange",
-  })
+    mode: 'onChange',
+  });
 
   const { fields, append } = useFieldArray({
-    name: "urls",
+    name: 'urls',
     control: form.control,
-  })
+  });
 
   function onSubmit(data: ProfileFormValues) {
     toast({
-      title: "You submitted the following values:",
+      title: 'You submitted the following values:',
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
 
   return (
@@ -97,21 +97,19 @@ export function SuppliersProfileForm() {
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
-              <FormDescription>
-                This is the company name
-              </FormDescription>
+              <FormDescription>This is the company name</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-       <FormField
+        <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
             <FormItem>
               <FormLabel>E-mail of the Organization</FormLabel>
               <FormControl>
-                <Input placeholder="example@gmail.com"  />
+                <Input placeholder="example@gmail.com" />
               </FormControl>
               <FormDescription>
                 Please Enter a valid E-mail address
@@ -120,23 +118,21 @@ export function SuppliersProfileForm() {
             </FormItem>
           )}
         />
-          <FormField
+        <FormField
           control={form.control}
           name="bio"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Location of the Organization</FormLabel>
               <FormControl>
-                <Input placeholder="Location of the Organization"  />
+                <Input placeholder="Location of the Organization" />
               </FormControl>
-              <FormDescription>
-                Please Enter a valid address
-              </FormDescription>
+              <FormDescription>Please Enter a valid address</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-       
+
         <FormField
           control={form.control}
           name="bio"
@@ -144,35 +140,31 @@ export function SuppliersProfileForm() {
             <FormItem>
               <FormLabel>Organization Type</FormLabel>
               <FormControl>
-                <Input placeholder="Private Company(LTD)"  />
+                <Input placeholder="Private Company(LTD)" />
               </FormControl>
               <FormDescription>
                 Please Enter if your company is private or public
               </FormDescription>
               <FormMessage />
             </FormItem>
-              )}
-            />
-            
-            
+          )}
+        />
+
         <FormField
           control={form.control}
           name="bio"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Please Enter Your Industry</FormLabel>
+              <FormLabel>Please Enter Your Contact Details</FormLabel>
               <FormControl>
-                <Input placeholder="Fabrication" />
+                <Input placeholder="Mobile No." />
               </FormControl>
-              <FormDescription>
-                Enter Your Industry Type
-              </FormDescription>
+              <FormDescription>Enter your number</FormDescription>
               <FormMessage />
             </FormItem>
-              )}
-            />
+          )}
+        />
 
-            
         <FormField
           control={form.control}
           name="bio"
@@ -180,16 +172,13 @@ export function SuppliersProfileForm() {
             <FormItem>
               <FormLabel>Year Of Incorporation</FormLabel>
               <FormControl>
-                <Input placeholder="1969"  />
+                <Input placeholder="1969" />
               </FormControl>
-              <FormDescription>
-                Please Enter a year 
-              </FormDescription>
+              <FormDescription>Please Enter a year</FormDescription>
               <FormMessage />
             </FormItem>
-              )}
-            />
-            
+          )}
+        />
 
         <FormField
           control={form.control}
@@ -198,57 +187,35 @@ export function SuppliersProfileForm() {
             <FormItem>
               <FormLabel>Receiving Purchase Orders </FormLabel>
               <FormControl>
-                <Input placeholder="Email,Bill,Invoices"  />
+                <Input placeholder="Email,Bill,Invoices" />
               </FormControl>
               <FormDescription>
                 Please Enter how to receive purchase orders
               </FormDescription>
               <FormMessage />
             </FormItem>
-              )}
-            />
-            
+          )}
+        />
 
-            
         <FormField
           control={form.control}
           name="bio"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Receiving Payments </FormLabel>
+              <FormLabel>Supplier Rating </FormLabel>
               <FormControl>
-                <Input placeholder="Cash,UPI,Etc..." />
+                <Input placeholder="0-5" />
               </FormControl>
               <FormDescription>
-                Please Enter how to receive payments
+                Please enter supplier rating
               </FormDescription>
               <FormMessage />
             </FormItem>
-              )}
-            />
-           
-            
-        <FormField
-          control={form.control}
-          name="bio"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Currencies </FormLabel>
-              <FormControl>
-                <Input placeholder="INR,USD"/>
-              </FormControl>
-              <FormDescription>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-              )}
-            />
-            
+          )}
+        />
 
-          
-        
         <Button type="submit">Create profile</Button>
       </form>
     </Form>
-  )
+  );
 }
